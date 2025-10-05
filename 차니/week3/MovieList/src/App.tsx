abef427db260f741560d864c1ca3dd09170ea037
Hyochan <hyochan02@gmail.com>
2025-10-05 16:04:35 +0900

    FEAT: 미션 2 - React Router와 페이지네이션을 통한 동적 영화 목록 구현

--- 차니/week3/MovieList/src/App.tsx
+++ 차니/week3/MovieList/src/App.tsx
@@ -1,12 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import MoviePage from "./pages/MoviePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import { MainLayout } from "./layouts/MainLayout";
+import { HomePage } from "./pages/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "movies/:category",
+        element: <MoviePage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <MoviePage />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
