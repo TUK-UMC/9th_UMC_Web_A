b34276dc18a0fee4306531a7711cc74e3a39bf97
성태경 <taegyeong0320@naver.com>
2025-11-19 11:52:33 +0900

    FEAT: 9주차 실습 1

---
+++ 신/week9/use-reducer/src/App.tsx
@@ -0,0 +1,12 @@
+import UseReducerCompany from "./pages/UseReducerCompany";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <UseReducerCompany />
+    </>
+  );
+}
+
+export default App;
