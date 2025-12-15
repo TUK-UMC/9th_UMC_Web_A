dedac675c5acc058bd76854c3e1c6d03a7a5348c
Hyochan <hyochan02@gmail.com>
2025-12-15 19:26:41 +0900

    FEAT: 미션 2. Vercel을 활용한 영화 사이트 배포 및 도메인 연결

--- 차니/week10/MovieSite/src/App.tsx
+++ 차니/week10/MovieSite/src/App.tsx
@@ -1,8 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import HomePage from "./assets/pages/HomePage";
+import MovieDetailPage from "./assets/pages/MovieDetailPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/:id",
+    element: <MovieDetailPage />,
+  },
+]);
 
 function App() {
-  return <HomePage />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
